0ce92305c9151752cc58a4ef6470cd6047cae555
kiminseob <kis6473@naver.com>
2023-05-25 01:32:53 +0900

    fix: naver logout 완전히 되도록 수정

--- src/components/common/Layout/AppBar/Logout.tsx
+++ src/components/common/Layout/AppBar/Logout.tsx
@@ -1,15 +1,29 @@
-import * as React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { LogoutIcon } from 'icons';
 import useStore from 'hooks/useStore';
 
 function Logout() {
   const { userInfoStore } = useStore();
-  const location = useLocation();
+  const navigate = useNavigate();
 
   const handleClick = () => {
     userInfoStore.logout();
-    window.location.replace(location.pathname);
+
+    const _window = window.open(
+      `https://nid.naver.com/nidlogin.logout`,
+      '_blank',
+      'width=100,height=100,scrollbars=no,status=no,toolbar=no,menubar=no,resizeable=no,location=no'
+    );
+
+    setTimeout(
+      (_window) => {
+        _window?.close();
+        navigate(0);
+      },
+      200,
+      _window
+    );
   };
 
   return <LogoutIcon onClick={handleClick} />;
